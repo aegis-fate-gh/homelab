3bc72a632c69d1d9d4c5c1d4e21bf75ec7221e8d
aegis-fate-gh <stephen.hudgins@gmail.com>
2024-04-06 15:01:09 -0500

    More Keepalived and host mode related changes

---
+++ EOS-Cluster/VMs/Swarm-Nodes/plex_check.sh
@@ -0,0 +1,8 @@
+#Put the below in: /usr/libexec/keepalived/
+
+#!/bin/bash
+
+docker ps -q -f name=plex_plex | grep -q . && exit 0 || exit 1
+
+# And then:
+# chmod u=rwx,g=rx,o=rx /usr/libexec/keepalived/jellyfin_check.sh
